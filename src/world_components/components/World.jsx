cf6b555b6e9f2edc9367bdf2e6361c3ea3cf9df5
ruhan <mdruhanahmed49@gmail.com>
2025-10-17 23:44:58 +0600

    card handling

--- src/world_components/components/World.jsx
+++ src/world_components/components/World.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const World = ({world}) => {
+const World = ({world,handleCard}) => {
     
     return (
         <div className='card'>
@@ -8,6 +8,7 @@ const World = ({world}) => {
             <h4>Name: {world.name}</h4>
             <p>Color: {world.color}</p>
             <p>Matrial: {world.material}</p>
+            <button onClick={()=>handleCard(world)}>Buy Now</button>
         </div>
     );
 };
--- src/world_components/components/Worlds.jsx
+++ src/world_components/components/Worlds.jsx
@@ -1,16 +1,24 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import World from './World';
 
 const Worlds = ({bottolsFech}) => {
+    const [card,setCard] = useState([])
+
     const worlds = use(bottolsFech)
+    const handleCard = (world)=>{
+        const newCard =[...card,setCard]
+        setCard(newCard)
+        
+    }
     
     
     return (
         <div>
             <h1>Hello World {worlds.length}</h1>
+            <p>Add Cart: {card.length}</p>
             <div className='grid'>
                 {
-                worlds.map(world=><World key={world.id} world={world}></World>)
+                worlds.map(world=><World key={world.id} world={world} handleCard={handleCard}></World>)
             }
             </div>
         </div>
